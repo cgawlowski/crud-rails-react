d833ffd6b56e5a4845fdddfa125bbc44fe84e3eb
Cyrille Gawlowski <gawlowski.cyrille@gmail.com>
2022-07-25 22:40:59 -0400

    Added routes + created CRUD methods in API:EventsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  resources :events
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      get 'events/index'
+      post 'events/create'
+      delete 'events/:id', to: 'events#destroy'
+    end
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'events#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
